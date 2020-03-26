e6aebca95ab5a4de1100afba132c9dd7a67f8759
Arthur Diniz <adl.csrss.exe1999@gmail.com>
2020-03-26 02:21:22 -0300

    controllers created

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,27 +1,13 @@
 // importing module dependencies
 const express = require("express")
+const ongController = require("./controllers/ONGController")
 const router = express()
-const crypto = require("crypto")
-const connection = require("./database/connection")
 
 // list existent ongs
-router.get("/ongs", async (req, res) => {
-    const ongs = await connection("ongs").select("*")
-
-    return res.json(ongs)
-})
+router.get("/ongs", ongController.index)
 
 // create new ong
-router.post("/ongs", async (req, res) => {
-    const {name, email, whatsapp, city, uf} = req.body
-    const id = crypto.randomBytes(4).toString("HEX")
-
-    await connection("ongs").insert({
-        id, name, email, whatsapp, city, uf
-    })
-
-    return res.json({id})
-})
+router.post("/ongs", ongController.create)
 
 // exports module's router
 module.exports = router
